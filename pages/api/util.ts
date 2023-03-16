86c8706c18c42d882993e65a52726dbe742c98b0
Sid Mohan <sidmohan001@gmail.com>
2023-03-16 00:01:26 -0700

    updated prompt

--- pages/api/util.ts
+++ pages/api/util.ts
@@ -14,7 +14,11 @@ const QA_PROMPT = PromptTemplate.fromTemplate(
   `You are an AI assistant trained to answer any questions about the tournament rules and regulations at the Presidio Golf & Concordia Club. this is the handbook https://https://pgccsf.com/getmedia/c544a939-6152-425e-80f8-f33827159a8a/PGCC_Golf_Handbook_04_1.aspx.io.
 You are given the following extracted parts of a long document and a question. Provide a conversational answer with a quote and page numbered citationj to the specific paragraph.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about the tournament rules at the Presidio Golf & Concordia Club, politely inform them that you are tuned to only answer questions about LangChain.
+If the question is not about the tournament rules at the Presidio Golf & Concordia Club, politely inform them that you are tuned to only answer questions about that topic.
+Here is a sample question/answer:
+Question: "When does the O'Shaughnessy Cup usually take place?
+Answer: "According to the PGCCSF guidebook on Page 7, the O'Shaughnessy Cup typically takes place on the 4th Saturday in March, though that is subject to change bu the Golf Committee.
+
 Question: {question}
 =========
 {context}

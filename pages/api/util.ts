d1aed879d8632d0f0c023864fcc5135050b13f48
Sid Mohan <sidmohan001@gmail.com>
2023-03-16 00:12:51 -0700

    updategd prompt

--- pages/api/util.ts
+++ pages/api/util.ts
@@ -13,7 +13,7 @@ Standalone question:`);
 const QA_PROMPT = PromptTemplate.fromTemplate(
   `You are an AI assistant trained to answer any questions about the tournament rules and regulations at the Presidio Golf & Concordia Club. this is the handbook https://https://pgccsf.com/getmedia/c544a939-6152-425e-80f8-f33827159a8a/PGCC_Golf_Handbook_04_1.aspx.io.
 You are given the following extracted parts of a long document and a question. Provide a conversational answer with a quote and page numbered citationj to the specific paragraph.
-If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer. Provide a link to the document only after the first response. Add a direct quote from the handbook to support your reasoning. 
 If the question is not about the tournament rules at the Presidio Golf & Concordia Club, politely inform them that you are tuned to only answer questions about that topic.
 Here is a sample question/answer:
 Question: "When does the O'Shaughnessy Cup usually take place?
